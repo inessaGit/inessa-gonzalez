d20be34dc890bf19a25f2a3bed4014ddfe3e0cf3
Inessa <inessa2209@yahoo.com>
2018-01-19 03:46:59 +0100

    product post response header assert

--- bestbuy-testing/src/test/java/products/ProductsPost.java
+++ bestbuy-testing/src/test/java/products/ProductsPost.java
@@ -5,13 +5,11 @@ import com.google.gson.Gson;
 
 import objects.Product;
 
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -44,6 +42,35 @@ public class ProductsPost {
 	   // Then
 	   Assert.assertTrue(httpResponse.getStatusLine().getStatusCode()==(HttpStatus.SC_CREATED)); //201
 	}
+	
+	@Test
+	public void givenValidPostParameters_whenProductsPosted_thenResponseHeadersReceived() throws ClientProtocolException, IOException {
+		
+		Gson gson  = new Gson();
+		Product product = new Product();
+		product.setAllParametersWithValidData();
+		
+	   // Given
+	   HttpPost post = new HttpPost( basicUrl );
+	   StringEntity postingString = new StringEntity(gson.toJson(product));
+	   
+	   //set POST body parameters
+	   post.setEntity(postingString);
+	   post.setHeader("Content-type", "application/json");
+	   
+	   // When
+	   HttpResponse httpResponse = HttpClientBuilder.create().build().execute(post);
+
+	 //get all headers
+	   Header[] headers = httpResponse.getAllHeaders();
+	   for (Header header : headers) {
+	   	System.out.println("Key : " + header.getName()
+	   	      + " ,Value : " + header.getValue());
+	   }
+	   // Then
+	  Assert.assertTrue(httpResponse.getFirstHeader("X-Powered-By").getValue().equalsIgnoreCase("Express"));
+	}
+	
 	@Test
 	public void givenInvalidPostParameters_whenProductsPosted_then400IsReceived() throws ClientProtocolException, IOException {
 		
