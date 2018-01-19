851ad113a5f539aed845e08d80449aab65607636
Inessa <inessa2209@yahoo.com>
2018-01-19 03:25:48 +0100

    produc post with missing parameters

--- bestbuy-testing/src/test/java/products/ProductsPost.java
+++ bestbuy-testing/src/test/java/products/ProductsPost.java
@@ -23,7 +23,7 @@ public class ProductsPost {
 	private String basicUrl="http://localhost:3030/products";
 	
 	@Test
-	public void givenValidEndpoint_whenProductsPosted_then201IsReceived() throws ClientProtocolException, IOException {
+	public void givenValidPostParameters_whenProductsPosted_then201IsReceived() throws ClientProtocolException, IOException {
 		
 		Gson gson  = new Gson();
 		Product product = new Product();
@@ -33,6 +33,7 @@ public class ProductsPost {
 	   HttpPost post = new HttpPost( basicUrl );
 	   StringEntity postingString = new StringEntity(gson.toJson(product));
 	   
+	   //set POST body parameters
 	   post.setEntity(postingString);
 	   post.setHeader("Content-type", "application/json");
 	   
@@ -42,5 +43,27 @@ public class ProductsPost {
 	   
 	   // Then
 	   Assert.assertTrue(httpResponse.getStatusLine().getStatusCode()==(HttpStatus.SC_CREATED)); //201
+	}
+	@Test
+	public void givenInvalidPostParameters_whenProductsPosted_then400IsReceived() throws ClientProtocolException, IOException {
+		
+		Gson gson  = new Gson();
+		Product product = new Product();
+		product.setDescription("test"); //other mandatory parameters missing 
+		
+	   // Given
+	   HttpPost post = new HttpPost( basicUrl );
+	   StringEntity postingString = new StringEntity(gson.toJson(product));
+	   
+	   //set POST body parameters
+	   post.setEntity(postingString);
+	   post.setHeader("Content-type", "application/json");
+	   
+	   // When
+	   HttpResponse httpResponse = HttpClientBuilder.create().build().execute(post);
+	   System.out.println(httpResponse.getStatusLine().getStatusCode());
+	   
+	   // Then
+	   Assert.assertTrue(httpResponse.getStatusLine().getStatusCode()==(HttpStatus.SC_BAD_REQUEST)); //400
 	}
 }
