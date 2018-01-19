1a72bfb564eb7dedcc7a5075a48ccea0e83d2786
Inessa <inessa2209@yahoo.com>
2018-01-19 02:12:43 +0100

    mime type test

--- bestbuy-testing/src/test/java/products/ProductsGet.java
+++ bestbuy-testing/src/test/java/products/ProductsGet.java
@@ -7,6 +7,7 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -23,6 +24,29 @@ public class ProductsGet {
 	   HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
 	   // Then
 	   Assert.assertTrue(httpResponse.getStatusLine().getStatusCode()==(HttpStatus.SC_OK)); //200
+	}
+	
+	@Test
+	public void givenValidEndpoint_whenProductRetrievedById_then200IsReceived() throws ClientProtocolException, IOException {
+	   // Given Get product with ID of 9132294
+	   HttpUriRequest request = new HttpGet( basicUrl + "/products/9132294" );
+	   // When
+	   HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+	   // Then
+	   Assert.assertTrue(httpResponse.getStatusLine().getStatusCode()==(HttpStatus.SC_OK)); //200
+	}
+	
+	@Test
+	public void givenValidEndpoint_whenProductsRetrieved_thenDefaultResponseContentTypeIsJson() throws ClientProtocolException, IOException {
+	   // Given Testing the Media Type
+	   HttpUriRequest request = new HttpGet( basicUrl + "/products" );
+	   // When
+	   HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+	   // Then
+	   String mimeType = ContentType.getOrDefault(httpResponse.getEntity()).getMimeType();
+
+	   System.out.println(mimeType);
+	   Assert.assertTrue(mimeType.equalsIgnoreCase(ContentType.APPLICATION_JSON.getMimeType())); 
 	}
 
 }
